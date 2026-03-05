83c0453309c08db3db9b68f5015d21ddfed2e792
Kamran <kamran@gildware.com>
2026-03-05 23:25:14 +0530

    Inital commit

---
+++ config/system-addons.php
@@ -0,0 +1,44 @@
+<?php return array (
+  'admin_panel' => 
+  array (
+    'active' => '1',
+    'name' => 'Gildware Technologies',
+    'identifier' => 'gildwaretechnologies@gmail.com',
+    'username' => 'gildwaretechnologies',
+    'purchase_key' => 'b2f34439-3306-4b59-becb-37f0808fb25c',
+    'software_id' => 'NDAyMjQ3NzI=',
+    'domain' => '127.0.0.1:8000',
+    'software_type' => 'product',
+    'errors' => 
+    array (
+    ),
+  ),
+  'provider_app' => 
+  array (
+    'active' => '0',
+    'name' => '',
+    'identifier' => '',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => '',
+    'errors' => 
+    array (
+    ),
+  ),
+  'serviceman_app' => 
+  array (
+    'active' => '',
+    'name' => '',
+    'identifier' => '',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => '',
+    'errors' => 
+    array (
+    ),
+  ),
+);
